d268a49245a976d3e689f65ded70fbab334c2867
Jaswa07 <jaswaramesh2005@gmail.com>
2024-02-23 17:36:14 +0530

    crud- handlers

---
+++ models/mobile.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const  mobileSchema = new mongoose.Schema({
+    
+    Manufacturer: String,
+    Specification: String,
+    Price: String,
+    Back_Camera: String,
+    Battery_Capacity: String,
+    Front_Camera: String,
+    Mobile_Model: String,
+    Operating_System: String,
+    Release_Year: Number,
+
+
+
+}); 
+
+const Mobile = mongoose.model("mobilemodels", mobileSchema)
+
+module.exports = Mobile;
