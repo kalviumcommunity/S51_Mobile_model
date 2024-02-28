3019cf18874428f5522f29e7c6fec8bc48700d55
Jaswa07 <jaswaramesh2005@gmail.com>
2024-02-28 12:28:02 +0530

    API fetching

--- client/react-app/src/App.jsx
+++ client/react-app/src/App.jsx
@@ -1,17 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './Components/Home'
-function App() {
-  const [count, setCount] = useState(0)
+import { useState, useEffect } from 'react'
 
-  return (
-    <>
-      <h1>Welcome</h1>
-      <Home/>
-    </>
-  )
-}
+import './App.css'
+// import Home from './Components/Home'
+function App() {
+  let [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const API = "https://mobile-models.onrender.com/models";
+    try {
+      const res = await fetch(API);
+      const responseData = await res.json();
+      setData(responseData);
+      console.log(responseData)
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+return (
+  <>
+    <h1>Welcome</h1>
+    <div className="container">
+      {data && data.map((ele, index) => (
+        <div key={index} className="box">
+          <h2>{ele.Manufacturer}</h2>
+          <p><strong>Specification:</strong> {ele.Specification}</p>
+          <p><strong>Price:</strong> {ele.Price} </p>
+          <p><strong>Back Camera:</strong> {ele.Back_Camera}</p>
+          <p><strong>Battery Capacity:</strong> {ele.Batter_Capacity}</p>
+          <p><strong>Front Camera:</strong> {ele.Front_Camera}</p>
+          <p><strong>Model:</strong> {ele.Mobile_Model}</p>
+          <p><strong>Operating System:</strong> {ele.Operating_System}</p>
+          <p><strong>Release Year:</strong> {ele.Release_Year}</p>
+        </div>
+      ))}
+    </div>
+  </>
+);
+      }
 
 export default App
