19375cf812ffbd2ed3a09d0bb505f8624aae7aa9
Jaswaa <141898693+Jaswaa@users.noreply.github.com>
2024-05-25 17:31:56 +0530

    Adding Filtering using Dropdown Menu

--- client/react-app/src/Components/Home.jsx
+++ client/react-app/src/Components/Home.jsx
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom"
 import axios from "axios"
 import "./AddData.css"
 function App({isauthorized}) {
+  const [filter,setFilter] = useState("All");
   const [data, setData] = useState([]);
   console.log(isauthorized, "home")
   useEffect(() => {
@@ -20,6 +21,14 @@ function App({isauthorized}) {
       console.error(err);
     }
   };
+  const filteredData = data.filter((item)=>{
+    if(filter === "All"){
+      return item
+    }
+    else if(item.Created_by.includes(filter)){
+      return item
+    }
+  })
   const deleteData = (id) =>{
     axios.delete(`https://mobile-models.onrender.com/delete/${id}`)
    .then((response) =>{ console.log(response.data);
@@ -35,9 +44,18 @@ function App({isauthorized}) {
     <>
       <h1>SmartPhones</h1>
       <Link to='/add'><button>Add</button></Link>
-
+      <div id="createdBy">
+      <p> Created By :   </p> 
+      <select name="createdBy" id="CreatedBy" onChange={(e)=>{setFilter(e.target.value)}}>
+              <option value="All">All</option>
+              <option value="Jaswant">Jaswant</option>
+              <option value="Chaithu">Chaithu</option>
+              <option value="Krithik">Krithik</option>
+              <option value="Harish">Harish</option>
+            </select>
+      </div>
       <div className="container">
-        {data && data.map((ele, index) => (
+        {filteredData && filteredData.map((ele, index) => (
 
           <div key={index} className="box">
             {/* Display data as before */}
@@ -51,6 +69,7 @@ function App({isauthorized}) {
         <p>Operating_System: {ele.Operating_System} </p>
         <p>Release_Year:{ele.Release_Year} </p>
         <p> Model Id :{ele.modelId}</p>
+        <p>Created_by:{ele.Created_by}</p>
         { isauthorized && <Link to={`/update/${ele.modelId}`} state={ele}><button id="update">Update</button></Link> }
         { isauthorized && <button onClick={()=>deleteData(ele._id)}>Delete</button>}
 
